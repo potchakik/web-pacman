6903ad68e5c00320c82e4f95048dde329bef6713
Dan Suba <DESuba@abs-cbn.com>
2023-01-16 14:06:25 +0800

    first commit

---
+++ game.js
@@ -0,0 +1,17 @@
+const canvas = document.getElementById("canvas");
+const canvasContext = canvas.getContext("2d");
+console.log(canvasContext);
+
+const pacmanFrames = document.getElementById("animation");
+const ghotsFrames = document.getElementById("ghosts");
+
+let createRect = (x, y, width, height, color) => {
+  canvasContext.fillStyle = color;
+  canvasContext.fillRect(x, y, width, height);
+};
+
+let map = [
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
